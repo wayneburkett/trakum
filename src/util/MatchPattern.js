641e2900d7a2923ee90ba33eb126d963707eb3f5
Wayne Burkett <wayne.burkett@gmail.com>
2020-06-03 00:31:43 -0400

    Expose the match pattern regex so we can use it for validation elsewhere

--- src/util/MatchPattern.js
+++ src/util/MatchPattern.js
@@ -1,15 +1,6 @@
 export const MatchPattern = (function() {
-    // ultimately, this should match as outlined here:
-    // https://developer.chrome.com/extensions/match_patterns
-    // this doesn't handle host names exhaustively (i.e. according to
-    // the examples there), but it's good enough for now
-    //
-    // the capturing groups here are designed to match JavaScript's
-    // URL components without additional formatting for comparison
-    const test = /^(http:|https:|\*:)\/\/([^/]+)(\/.+)$/
-
     function components (pattern) {
-        const results = pattern.match(test)
+        const results = pattern.match(matcher.MATCH_PATTERN_REGEX)
         if (!results || results.length !== 4) throw new Error('Invalid match pattern')
         return results.slice(1).map(c => convertToRegExp(c))
     }
@@ -53,12 +44,22 @@ export const MatchPattern = (function() {
         return new RegExp(`^${res}$`, 'i')
     }
 
-    return (pattern) => {
+    function matcher(pattern) {
         const [protocol, host, path] = components(pattern)
-
         return (url) =>
             protocol.test(url.protocol) &&
             host.test(url.hostname) &&
             path.test(url.pathname + url.search)
     }
+
+    // ultimately, this should match as outlined here:
+    // https://developer.chrome.com/extensions/match_patterns
+    // this doesn't handle host names exhaustively (i.e. according to
+    // the examples there), but it's good enough for now
+    //
+    // the capturing groups here are designed to match JavaScript's
+    // URL components without additional formatting for comparison
+    matcher.MATCH_PATTERN_REGEX = /^(http:|https:|\*:)\/\/([^/]+)(\/.+)$/
+
+    return matcher
 })()
