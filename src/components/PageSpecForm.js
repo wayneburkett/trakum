895ff22c1d53628828917654d98e5de64f6933ca
Wayne Burkett <wayne.burkett@gmail.com>
2020-06-06 22:58:55 -0400

    Formatting

--- src/components/PageSpecForm.js
+++ src/components/PageSpecForm.js
@@ -13,8 +13,8 @@ const schema = Yup.object({
   query: Yup.string().required('Query is required')
 })
 
-function PageSpecForm() {
-  const [query, setQuery] = useState("")
+function PageSpecForm () {
+  const [query, setQuery] = useState('')
   const { currentUrl, tabId, addPageSpec } = useContext(GlobalContext)
 
   const handleSubmit = async evt => {
@@ -49,38 +49,38 @@ function PageSpecForm() {
         values,
         touched,
         isInvalid,
-        errors,
+        errors
       }) => (
         <Form onSubmit={handleSubmit}>
           <Form.Row>
-            <Form.Group as={Col} md="4" controlId="validationFormik01">
+            <Form.Group as={Col} md='4' controlId='validationFormik01'>
               <Form.Control
-                type="text"
-                placeholder="Match pattern"
-                name="pattern"
+                type='text'
+                placeholder='Match pattern'
+                name='pattern'
                 value={values.pattern}
                 onChange={handleChange}
                 isInvalid={!!errors.pattern}
               />
-              <Form.Control.Feedback type="invalid">{errors.pattern}</Form.Control.Feedback>
+              <Form.Control.Feedback type='invalid'>{errors.pattern}</Form.Control.Feedback>
             </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormik02">
+            <Form.Group as={Col} md='4' controlId='validationFormik02'>
               <Form.Control
-                type="text"
-                placeholder="Query"
-                name="query"
+                type='text'
+                placeholder='Query'
+                name='query'
                 value={values.query}
-                onChange={(e) => { handleChange(e); handlePatternChange(e); }}
+                onChange={(e) => { handleChange(e); handlePatternChange(e) }}
                 isInvalid={!!errors.query}
               />
-              <Form.Control.Feedback type="invalid">{errors.query}</Form.Control.Feedback>
+              <Form.Control.Feedback type='invalid'>{errors.query}</Form.Control.Feedback>
             </Form.Group>
           </Form.Row>
-          <Button type="submit" variant="dark">Save</Button>
+          <Button type='submit' variant='dark'>Save</Button>
         </Form>
       )}
     </Formik>
-  );
+  )
 }
 
 export default PageSpecForm
