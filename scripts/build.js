a4930b1688eed1050670213a80f4edbfb0acea24
Wayne Burkett <wayne.burkett@gmail.com>
2020-07-08 00:38:51 -0400

    Rename the main build products

--- scripts/build.js
+++ scripts/build.js
@@ -13,3 +13,7 @@ config.optimization.splitChunks = {
 
 config.optimization.runtimeChunk = false
 
+// create predictable output files without hashes in the name
+config.output.filename = 'static/js/[name].js'
+config.plugins[5].options.filename = 'static/css/[name].css'
+config.plugins[5].options.moduleFilename = () => 'static/css/main.css'
