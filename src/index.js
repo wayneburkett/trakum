724d7dd6e40b336c18e014013ba1162df0ddbba3
Wayne Burkett <wayne.burkett@gmail.com>
2020-11-29 01:05:43 -0500

    Add an explanation for the seemingly-random div insertion

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App'
 import { GlobalProvider } from './context/GlobalState'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// create a div and insert it before the first element in the document; this
+// is so that we can inject the react app even when we don't know what's on the
+// page (e.g. in a content script)
 const anchor = document.createElement('div')
 anchor.id = 'trakum-app'
 document.body.insertBefore(anchor, document.body.childNodes[0])
