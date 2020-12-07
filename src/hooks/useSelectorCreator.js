b53c37bdc8883ddf78d5ddc2b165e80929d89aeb
Wayne Burkett <wayne.burkett@gmail.com>
2020-12-06 23:45:44 -0500

    Add useSelectorCreator hook

---
+++ src/hooks/useSelectorCreator.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import { createSelector } from '../util/Selectors'
+
+const useSelectorCreator = (eventName = 'mouseover', toggle = false) => {
+  const [selector, setSelector] = useState({ x: null, y: null, sel: null })
+
+  const updateSelector = ev => {
+    const currentSelector = (toggle && selector.sel) ? null : createSelector(ev.target)
+    setSelector({ x: ev.clientX, y: ev.clientY, sel: currentSelector })
+  }
+
+  useEffect(() => {
+    window.addEventListener(eventName, updateSelector)
+    return () => window.removeEventListener(eventName, updateSelector)
+  }, [])
+
+  return selector
+}
+
+export default useSelectorCreator
