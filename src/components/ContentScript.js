a0a05ddfc82a03935c2dddec4be3a52f5c2c8230
Wayne Burkett <wayne.burkett@gmail.com>
2020-12-06 23:40:35 -0500

    Move some of selector creation into React
    Convert the useMousePosition hook into something that can be used to
    create a selector for any event and then use this to create and display
    selectors on mouseover and clicks.
    
    This functionality can now be moved out of picker.js (and ultimately
    everything else in there should be moved into React, as well).

--- src/components/ContentScript.js
+++ src/components/ContentScript.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import useMousePosition from '../hooks/useMousePosition'
+import Button from 'react-bootstrap/Button'
+import useSelectorCreator from '../hooks/useSelectorCreator'
 
 export const ContentScript = () => {
-  const { x, y, sel } = useMousePosition()
+  const { x, y, sel: selector1 } = useSelectorCreator('mouseover')
+  const { sel: selector2 } = useSelectorCreator('click', true)
   const hasMovedCursor = typeof x === 'number' && typeof y === 'number'
 
+  const handleClick = ev => {
+    ev.preventDefault()
+  }
+
   return (
-    <div>
+    <div onClick={handleClick}>
       {hasMovedCursor
-        ? `${sel}`
+        ? `${selector2} and ${selector1}`
         : 'Move your mouse'}
+      <Button as="input" type="reset" value="Reset" onClick={handleClick} />
     </div>
   )
 }
